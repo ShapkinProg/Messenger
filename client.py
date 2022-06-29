9a06da294a8f9d3cf9990ef6d7a9854dc839d13c
ShapkinProg <shlya12332q1@yandex.ru>
2022-06-29 04:49:28 +0300

    Working messenger, without file sharing Needs functionality testing for bugs

--- client.py
+++ client.py
@@ -6,17 +6,62 @@ import json
 import os.path
 from datetime import datetime
 import os
+import time
+len_field = 60#максиамльная длина строки в чате
+half_len_field = int(len_field/2)#половина строки в чате
+
+
+def commands():
+    print("Для обновления переписки отправьте любое сообщение либо используйте команду /update")
+    print("Для возврата назад и выбора другого пользователя напише /back")
+    print("Для выхода из приложения используйте /exit")
+
+
+def print_history():#печать истории диалога
+    dialog = json.loads(client.recv(4096).decode())  # получаем историю
+    print('Вы' + ' ' * (len_field - 2 - len(name_original)) + name_original)
+    print("_" * len_field)
+    for i in dialog.get('messages'):  # выводим её
+        if i.get("author") == name_original:#вывод в правой части консоли пользователя с которым идёт переписка
+            if len(i.get('massage')) < half_len_field:
+                print(' ' * int(len_field - len(i.get('massage'))) + i.get('massage') + '\n')
+            else: #если соообщение большое то разбиваем его на несколько строк
+                numbers_n = float(len(i.get('massage')) / half_len_field)
+                range_cycle = int(numbers_n)
+                if numbers_n != int(numbers_n):
+                    range_cycle = int(numbers_n) + 1
+                for number in range(range_cycle):
+                    if number == range_cycle and range_cycle == int(numbers_n):
+                        print(' ' * int(len_field - len(i.get('massage'))) + i.get('massage') + '\n')
+                    else:
+                        print(" " * half_len_field + i.get('massage')[
+                                                     number * half_len_field:(number + 1) * half_len_field])
+        else: #вывод влевой части консоли залогиненого пользователя
+            if len(i.get('massage')) < half_len_field:
+                print(i.get('massage') + '\n')
+            else:   #если соообщение большое то разбиваем его на несколько строк
+                numbers_n = float(len(i.get('massage')) / half_len_field)
+                range_cycle = int(numbers_n)
+                if numbers_n != int(numbers_n):
+                    range_cycle = int(numbers_n) + 1
+                for number in range(range_cycle):
+                    if number == range_cycle and range_cycle == int(numbers_n):
+                        print(i.get('massage') + '\n')
+                    else:
+                        print(i.get('massage')[number * half_len_field:(number + 1) * half_len_field])
+
 
 if __name__ == '__main__':
-
+    clear_console = lambda: os.system('cls') #если очистка консоли не работает в ide, то включите эмуляцию терминала
+                                                # run->Edit Configurations->Emulate terminal output
     SERVER = "127.0.0.1"
     PORT = 1337
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #открывание сокета, нужно будет сделать обёртку для TLS
     client.connect((SERVER, PORT))
-
-    while True:
+    while True: #вход в ситему
         print("[1] Login\n[2]Sign in")
         res = input()
+        clear_console()
         try:
             if int(res) == 1 or int(res) == 2:
                 print("Введите ваш логин:")
@@ -31,27 +76,34 @@ if __name__ == '__main__':
                 client.sendall(bytes(send_login, 'UTF-8'))
                 ans = client.recv(4096).decode() #ждём результат запроса
                 if ans == '1':
+                    clear_console()
                     print("Вы успшно вошли")
                     break
                 elif ans == '-1':
+                    clear_console()
                     print("Такой логин уже занят")
                 elif ans == '-2':
+                    clear_console()
                     print("Что то пошло не так, поробуйте снова")
                 else:
+                    clear_console()
                     print("Неверный логин или пароль")
                     continue
             else:
+                clear_console()
                 print("Введены неверные параметры, попробуйте снова")
         except:
+            clear_console()
             print("Введены неверные параметры, попробуйте снова")
 
     print("Введите имя пользователя которому хотите написать:") #после логина ищем чубрика которому хотим написать
-    while True:
+    while True: #Основное 'меню' чата
         name = input()
         name_original = name
         name = 'F' + name + " " + login
         name = cryptocode.encrypt(name, 'key') #шифруем и отпарвляем на сервер
         client.sendall(bytes(name, 'UTF-8'))
+
         ans = client.recv(4096).decode() #ждём овтета
         if ans == "0": #не смогли найти чела точно по имени
             ans = json.loads(client.recv(4096).decode()) #json файл со всеми челиками примерно сопадающими по имени
@@ -59,19 +111,43 @@ if __name__ == '__main__':
                 print("Результаты по запросу " + name_original + ":")
                 print(ans.get("users"))
             else: #если файл пустой то пользователей нет
+                clear_console()
                 print("Такого пользователя нет, попытайтесь ещё раз")
         elif ans == "1": #если нашли пользователя по имени
+            clear_console()
             mode = client.recv(4096).decode()
-            if mode == '1':
-                dialog = json.loads(client.recv(4096).decode())
-                for i in dialog.get('messages'):
-                    print(i.get('massage'))
-                # print(dialog.get('messages')[0].get('massage'))
+            if mode == '1': #если есть история диалога
+                print_history() #выводим историю
+            print("Чтобы узнать команды отправьте /help")
             print("Введите сообщение:")
             while True: #начинаем диалог
                 message = input()
+                if message == '/help':
+                    commands()
+                    continue
+                elif message == '/update':
+                    client.sendall(bytes(cryptocode.encrypt("R" + login + " " + name_original, 'key'), 'UTF-8'))
+                    mode = client.recv(4096).decode()
+                    clear_console()
+                    print_history()  # выводим обновлённый диалог
+                    print("Введите сообщение:")
+                    continue
+                elif message == '/back':
+                    clear_console()
+                    print("Введите имя пользователя которому хотите написать:")
+                    break
+                elif message == '/exit':
+                    print("Надеюсь ещё увидимся. До свидания)")
+                    exit()
+
                 info = "M" + login + " " + name_original
                 info = cryptocode.encrypt(info, 'key')  # шифруем и отпарвляем на сервер
-                client.sendall(bytes(info, 'UTF-8'))
-                client.sendall(bytes(message, 'UTF-8'))
-
+                client.sendall(bytes(info, 'UTF-8')) #отпаряем данные кто и кому
+                client.sendall(bytes(message, 'UTF-8'))#отпарляем сообщение
+                time.sleep(0.5)
+                client.sendall(bytes(cryptocode.encrypt("R"+login+" "+name_original, 'key'), 'UTF-8'))#отпарялем запрос на обноваление диалога
+                mode = client.recv(4096).decode() #ответ 1 - есть диалог 0 - диалог пустой
+                if mode == "1":
+                    clear_console()
+                    print_history()#выводим обновлённый диалог
+                    print("Введите сообщение:")
--- server.py
+++ server.py
@@ -15,9 +15,9 @@ class ClientThread(threading.Thread):
         self.csocket = clientsocket
         print("Новое подключение: ", clientAddress)
 
-    def check_unique(self, login):
+    def check_unique(self, login): #функция првоерки на уникалдьный логин
         try:
-            sqlite_connection = sqlite3.connect('resources/sqlite_python.db')
+            sqlite_connection = sqlite3.connect('resources/sqlite_python.db') #получаем даныне с бд
             cursor = sqlite_connection.cursor()
             cursor.execute('SELECT * FROM users')
             data = cursor.fetchall()
@@ -26,7 +26,7 @@ class ClientThread(threading.Thread):
             if sqlite_connection:
                 cursor.close()
             return -1
-        for i in data:
+        for i in data: #проверяем есть ли такой же логин
             if i[1] == login:
                 return 0
         return 1
@@ -34,23 +34,24 @@ class ClientThread(threading.Thread):
     def run(self):
         msg = ''
         while True:
-            data = self.csocket.recv(4096)
+            data = self.csocket.recv(4096) #получение сообщений от пользователя
             msg = data.decode()
-            msg = cryptocode.decrypt(msg, 'key')
+            msg = cryptocode.decrypt(msg, 'key') #расшифровка сообщений
             if msg == '' or msg == False:  # проверка на пустое сообщение -> пользователь отключился
                 print("Отключение")
                 break
             if msg[0] == "L":  # логин
-                # Поиск данных в БД
+                #парс полученный строки с данными
                 login = msg[1:msg.find(" ")]
                 password = msg[msg.find(" ") + 1:]
+                # Поиск данных в БД
                 try:
                     sqlite_connection = sqlite3.connect('resources/sqlite_python.db')
                     cursor = sqlite_connection.cursor()
                     cursor.execute('SELECT * FROM users WHERE login=?', (login,))
                     data = cursor.fetchall()
                     cursor.close()
-                    if data[0][2] == password:
+                    if data[0][2] == password: #проверка пароля по бд
                         self.csocket.send(bytes("1", 'UTF-8'))
                     else:
                         self.csocket.send(bytes("0", 'UTF-8'))
@@ -86,10 +87,10 @@ class ClientThread(threading.Thread):
                     if sqlite_connection:
                         cursor.close()
 
-            elif msg[0] == "F":  # поиск пользователя по бд
+            elif msg[0] == "F":
+                #парс строки с данными от пользователя
                 name = msg[1:msg.find(" ")]
                 login = msg[msg.find(" ") + 1:]
-
                 sqlite_connection = sqlite3.connect('resources/sqlite_python.db') # получение всех пользователей
                 cursor = sqlite_connection.cursor()
                 cursor.execute('SELECT login FROM users')
@@ -98,12 +99,12 @@ class ClientThread(threading.Thread):
 
                 users = []
                 flag = True
-                for i in data:
+                for i in data:      # поиск пользователя по бд
                     if i[0] == name:
                         self.csocket.send(bytes("1", 'UTF-8'))
                         flag = False
-                        if os.path.isfile(f'repo/{login}/{name}.json'):
-                            time.sleep(0.5)
+                        if os.path.isfile(f'repo/{login}/{name}.json'): #если есть диалог с этим пользователем отправляем его
+                            time.sleep(0.5) #задержка перед отправкой данных, чтобы они не слипались
                             self.csocket.send(bytes("1", 'UTF-8'))
                             time.sleep(0.5)
                             with open(f'repo/{login}/{name}.json', "r", encoding='utf8') as file:
@@ -114,20 +115,22 @@ class ClientThread(threading.Thread):
                             # load = []
                             # self.csocket.send(json.dumps({"messages": load}).encode())
                         break
-                    if i[0].find(name) != -1:
-                        flag = True
+                    if i[0].find(name) != -1:#если не нашли точно по имени, то добавляем пользователей которые схожи с запросом
+                        flag = True          #и отпраляем пользователю
                         users.append(i[0])
                 if flag:
                     self.csocket.send(bytes("0", 'UTF-8'))
                     self.csocket.send(json.dumps({"users": users}).encode())
             elif msg[0] == "M":  # Диалог с пользователем (запись в бд на сервере)
-                from_user = msg[1:msg.find(" ")]
-                to_user = msg[msg.find(" ") + 1:]
-                massage = self.csocket.recv(4096).decode()
+                #парсим сообщение от пользователя
+                from_user = msg[1:msg.find(" ")] #от кого
+                to_user = msg[msg.find(" ") + 1:]#кому
+                massage = self.csocket.recv(4096).decode()#само сообщение
 
-                if os.path.isfile(f'repo/{from_user}/{to_user}.json'):  # если диалог уже есть
+                if os.path.isfile(f'repo/{from_user}/{to_user}.json'):  # если диалог уже есть записываем данные
                     ###################################### для отправителя
                     data = {
+                            'author':from_user,
                             'massage': massage,
                             'data': datetime.now().strftime("%H:%M %d/%m/%Y")}
                     with open(f'repo/{from_user}/{to_user}.json', "r", encoding='utf8') as file:
@@ -137,6 +140,7 @@ class ClientThread(threading.Thread):
                         json.dump(load, f, indent=4, ensure_ascii=False)
                     ###################################### для того кто получает
                     data = {
+                            'author': from_user,
                             'massage': massage,
                             'data': datetime.now().strftime("%H:%M %d/%m/%Y")}
                     with open(f'repo/{to_user}/{from_user}.json', "r", encoding='utf8') as file:
@@ -144,22 +148,36 @@ class ClientThread(threading.Thread):
                     load.append(data)
                     with open(f'repo/{to_user}/{from_user}.json', 'w', encoding='utf8') as f:
                         json.dump(load, f, indent=4, ensure_ascii=False)
-                else:  # если диаолога ещё не было
-
-                    os.mkdir(f'repo/{from_user}')
-                    os.mkdir(f'repo/{to_user}')
+                else:  # если диаолога ещё не было, то создаём файлы для записи и записываем
+                    if not os.path.exists(f'repo/{from_user}'):
+                        os.mkdir(f'repo/{from_user}')
+                    if not os.path.exists(f'repo/{to_user}'):
+                        os.mkdir(f'repo/{to_user}')
                     ###################################### для отправителя
                     data = [{
+                            'author': from_user,
                              'massage': massage,
                              'data': datetime.now().strftime("%H:%M %d/%m/%Y")}]
                     with open(f'repo/{from_user}/{to_user}.json', 'w', encoding='utf8') as f:
                         json.dump(data, f, indent=4, ensure_ascii=False)
                     ###################################### для того кто получает
                     data = [{
+                            'author': from_user,
                              'massage': massage,
                              'data': datetime.now().strftime("%H:%M %d/%m/%Y")}]
                     with open(f'repo/{to_user}/{from_user}.json', 'w', encoding='utf8') as f:
                         json.dump(data, f, indent=4, ensure_ascii=False)
+            elif msg[0] == 'R':
+                login = msg[1:msg.find(" ")] #от кого
+                name = msg[msg.find(" ") + 1:]#кому
+                if os.path.isfile(f'repo/{login}/{name}.json'):  # если есть диалог с этим пользователем отправляем его
+                    self.csocket.send(bytes("1", 'UTF-8'))
+                    time.sleep(0.5)                             # задержка перед отправкой данных, чтобы они не слипались
+                    with open(f'repo/{login}/{name}.json', "r", encoding='utf8') as file:
+                        load = json.load(file)
+                    self.csocket.send(json.dumps({"messages": load}).encode())
+                else:
+                    self.csocket.send(bytes("0", 'UTF-8'))
 
 
 if __name__ == '__main__':
@@ -174,4 +192,4 @@ if __name__ == '__main__':
         server.listen(10)  # слушаем одновременно 10 челиков
         clientsock, clentAddress = server.accept()  # ждём нового подключения
         newthread = ClientThread(clentAddress, clientsock)  # в новом потоке обрабатываем пользователя
-        newthread.start()
+        newthread.start() #запускаем поток
